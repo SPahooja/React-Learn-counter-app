d0537852c10dba0d27ac25fa7a5eac30f1d60fde
SPahooja <pahoojasahil@gmail.com>
2020-08-30 08:39:53 +0530

    added parent for counters

---
+++ src/components/counters.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  render() {
+    const { onReset, onIncrement, onDelete, counters } = this.props;
+    return (
+      <div>
+        <div className="button btn btn-primary btn-sm m-2" onClick={onReset}>
+          Reset
+        </div>
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
